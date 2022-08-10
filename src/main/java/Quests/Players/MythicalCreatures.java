3c84aaf56b2162a8fb3cecb064ab0fc8b51004a3
Cetins <69481095+Cetins@users.noreply.github.com>
2022-08-10 12:01:05 +0100

    mythical creatures added

---
+++ src/main/java/Quests/Players/MythicalCreatures.java
@@ -0,0 +1,18 @@
+package Quests.Players;
+
+public enum MythicalCreatures {
+
+    OGRE(10, 4, 100),
+    WOLF(30, 2, 300),
+    DRAGON(40,1, 400);
+
+    private final int damage;
+    private final int speed;
+    private final int health;
+
+    MythicalCreatures(int damage, int speed, int health) {
+        this.damage = damage;
+        this.speed = speed;
+        this.health = health;
+    }
+}
